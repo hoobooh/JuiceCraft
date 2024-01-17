5ff9232b3eee9aafd9dedbf1086f3b4ec9cf181d
hoobooh <hoobooh47@gmail.com>
2024-01-17 12:57:15 -0800

    swim effects

--- src/main/java/com/usagin/juicecraft/ai/goals/FriendFloatGoal.java
+++ src/main/java/com/usagin/juicecraft/ai/goals/FriendFloatGoal.java
@@ -4,6 +4,7 @@ import com.mojang.logging.LogUtils;
 import com.usagin.juicecraft.friends.Friend;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Pose;
@@ -35,7 +36,7 @@ public class FriendFloatGoal extends FloatGoal {
 
         //|| this.friend.isInFluidType((fluidType, height) -> this.friend.canSwimInFluidType(fluidType) && height > this.friend.getFluidJumpThreshold());
     }
-
+    public int swimsound = 7;
     public void tick() {
         //LOGGER.info(this.friend.getSurfaceWaterDistanceFromEye() +"");
         if (this.friend.getSurfaceWaterDistanceFromEye() < 0.2) {
@@ -45,6 +46,27 @@ public class FriendFloatGoal extends FloatGoal {
             double change = (double) 0.04F * this.friend.getAttributeValue(ForgeMod.SWIM_SPEED.get());
             this.friend.setDeltaMovement(this.friend.getDeltaMovement().add(0.0D, change, 0.0D));
         }
+
+        if(Math.sqrt(this.friend.getDeltaMovement().x * this.friend.getDeltaMovement().x + this.friend.getDeltaMovement().z * this.friend.getDeltaMovement().z) > 0.03){
+            if(this.swimsound%10==0){
+                if(this.friend.level() instanceof ServerLevel level){
+                    level.sendParticles(ParticleTypes.SPLASH,this.friend.getX(),this.friend.getY()+1,this.friend.getZ(),5,0.25,0,0.25,1);
+                }
+                this.friend.playSound(SoundEvents.PLAYER_SPLASH,0.05F,Mth.nextFloat(this.friend.getRandom(),0.8F,1.1F));
+            }
+        }
+            if(this.swimsound<=0){
+                if(this.friend.level() instanceof ServerLevel level){
+                    level.sendParticles(ParticleTypes.SPLASH,this.friend.getX(),this.friend.getY()+1,this.friend.getZ(),5,0.25,0,0.25,1);
+                }
+                this.friend.playSound(SoundEvents.PLAYER_SWIM,0.05F,Mth.nextFloat(this.friend.getRandom(),0.8F,1.1F));
+                this.swimsound=30;
+            }
+            else{
+                this.swimsound--;
+            }
+
+
         if (this.friend.getSurfaceWaterDistanceFromEye() > 0.45) {
             this.friend.setFriendSwimCounter(11);
         }
