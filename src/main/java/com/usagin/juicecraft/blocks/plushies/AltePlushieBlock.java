68ae4a320792eb7e8949fc8861aa1f0caa44a803
hoobooh <hoobooh47@gmail.com>
2024-02-12 15:48:41 -0800

    block shape

--- src/main/java/com/usagin/juicecraft/blocks/plushies/AltePlushieBlock.java
+++ src/main/java/com/usagin/juicecraft/blocks/plushies/AltePlushieBlock.java
@@ -2,9 +2,15 @@ package com.usagin.juicecraft.blocks.plushies;
 
 import com.usagin.juicecraft.JuiceCraft;
 import net.minecraft.client.resources.model.Material;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class AltePlushieBlock extends PlushieBlock {
 
@@ -16,5 +22,8 @@ public class AltePlushieBlock extends PlushieBlock {
     @Override
     public Material getMaterial() {
         return new Material(ATLAS,ATLAS);
+    }
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return PLUSHIESHAPE;
     }
 }
--- src/main/java/com/usagin/juicecraft/blocks/plushies/PlushieBlock.java
+++ src/main/java/com/usagin/juicecraft/blocks/plushies/PlushieBlock.java
@@ -2,11 +2,14 @@ package com.usagin.juicecraft.blocks.plushies;
 
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CandleBlock;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public abstract class PlushieBlock extends Block {
 
     public PlushieBlock(Properties pProperties) {
         super(pProperties);
     }
+    public static VoxelShape PLUSHIESHAPE = Block.box(6.0D, 0.0D, 6.0D, 10.0D, 6.0D, 10.0D);
     public abstract Material getMaterial();
 }
