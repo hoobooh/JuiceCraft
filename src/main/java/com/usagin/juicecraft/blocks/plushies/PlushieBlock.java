3c137c238e4473294fce348cb4a43b9ba264bead
hoobooh <hoobooh47@gmail.com>
2024-02-13 04:19:19 -0800

    recipes

--- src/main/java/com/usagin/juicecraft/blocks/plushies/PlushieBlock.java
+++ src/main/java/com/usagin/juicecraft/blocks/plushies/PlushieBlock.java
@@ -3,9 +3,10 @@ package com.usagin.juicecraft.blocks.plushies;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CandleBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public abstract class PlushieBlock extends Block {
+public abstract class PlushieBlock extends HorizontalDirectionalBlock {
 
     public PlushieBlock(Properties pProperties) {
         super(pProperties);
